094ddc6b03b5abb72cce9634d919e4860097a687
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-15 18:19:28 +0530

    identity of matrics

---
+++ identity_matrices.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# identity matrices eye(size) 
+
+identy_matrix = np.eye(3)
+print(identy_matrix)
+
+# multyplication of two identity matrices
+identy_matrix1 = np.eye(3)
+identy_matrix2 = np.eye(3)
+print(identy_matrix1 * identy_matrix2)
