b2606bafc5e7e16aad0425d8269bdecbfab58f0e
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-16 00:17:58 +0530

    fancy indexing in numpy array

--- slice.py
+++ slice.py
@@ -1,10 +1,14 @@
-import pandas as pd
+import numpy as np
 
 # array[start:stop:step]
 
-arry = [10,20,30,40,50,60]
+arry = np.array([10,20,30,40,50,60])
 print(arry[2:]) # indexing 2 to end
 print(arry[1:5]) # indexing 1 to 4
 print(arry[:4]) # indexing 0 to 4
 print(arry[::2]) # indexing 0 to end by remove 2 stps 
 print(arry[::-1]) # print array right to left
+
+# fancy indexing
+
+print(arry[[0,2,5]])
