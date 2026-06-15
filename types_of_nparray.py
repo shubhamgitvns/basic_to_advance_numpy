2ba97b2d40f7b26e2326819eda924937b0b94523
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-15 18:14:53 +0530

    types of array in numpy

---
+++ types_of_nparray.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# zero array
+# 1d in array
+zerro_array = np.zeros(3)
+print(zerro_array)
+
+# 2d in zerroarray
+zerro_array_2d = np.zeros((2,2))
+print(zerro_array_2d)\
+
+# one array
+
+ones_array = np.ones((2,3))
+print(ones_array)
+
+# full (shape,value)
+
+full_array = np.full((2,2),7)
+print(full_array)
+
+
