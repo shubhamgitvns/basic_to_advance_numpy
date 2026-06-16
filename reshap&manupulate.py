18b78616be7886da71df1d1218b8db8b95d426be
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-16 19:15:57 +0530

    commant the work of reshape in 1d array.

--- reshap&manupulate.py
+++ reshap&manupulate.py
@@ -1,7 +1,7 @@
-
+# the reshape convert the 1d array in 2d and  3d 
 import numpy as np
 
 array = np.array([1,2,3,4,5,6,])
 
-reshap = array.reshape(3,2)
+reshap = array.reshape(3,2) # reshapee(row,column)
 print(reshap)
